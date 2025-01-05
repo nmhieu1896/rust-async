8630bd44e1ce2e823f37e99f82277c404cf0aeb7
minhhieu1896 <minhhieu1896@gmail.com>
2025-01-05 21:08:48 +0700

    ch3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::thread::{self, sleep};
+
 fn main() {
-    println!("Hello, world!");
+    println!("So, we start the program here!");
+    let t1 = thread::spawn(|| {
+        sleep(std::time::Duration::from_millis(200));
+        println!("The long running tasks finish last!");
+        return 1;
+    });
+
+    let t2 = thread::spawn(|| {
+        sleep(std::time::Duration::from_millis(100));
+        println!("We can chain callbacks...");
+        let t3 = thread::spawn(move || {
+            sleep(std::time::Duration::from_millis(50));
+            println!("...like this!");
+            return 2;
+        });
+        return t3.join().unwrap();
+    });
+    println!("The tasks run concurrently!");
+
+    let res1 = t1.join().unwrap();
+    let res2 = t2.join().unwrap();
+    println!("{} - {}", res1, res2)
 }
